06d8fe29469d6d9ea92e7f2c9af7c775019b6f20
caohaidang-work <caohaidang05.work@gmail.com>
2026-04-22 13:58:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get("/", (req, res) => {
   res.send("Server OK");
 });
 
+// 👉 TEST LINK THANH TOÁN
+app.get("/pay", (req, res) => {
+  res.send("Thanh toán OK (test bước 1)");
+});
+
 app.listen(3000, () => console.log("Server running"));
