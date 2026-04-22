30e2813c437440e8294ba231f98784908f2d6dbc
caohaidang-work <caohaidang05.work@gmail.com>
2026-04-22 14:03:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ app.get("/", (req, res) => {
   res.send("Server OK");
 });
 
-// 👉 TEST LINK THANH TOÁN
+const crypto = require("crypto");
+const qs = require("qs");
+
 app.get("/pay", (req, res) => {
-  res.send("Thanh toán OK (test bước 1)");
+
+    const vnp_TmnCode = "TESTCODE";
+    const vnp_HashSecret = "SECRET";
+
+    let params = {
+        vnp_Version: "2.1.0",
+        vnp_Command: "pay",
+        vnp_TmnCode: vnp_TmnCode,
+        vnp_Amount: 10000 * 100,
+        vnp_CurrCode: "VND",
+        vnp_TxnRef: "TEST123",
+        vnp_OrderInfo: "Test thanh toan",
+        vnp_OrderType: "other",
+        vnp_Locale: "vn",
+        vnp_ReturnUrl: "vnpay-odoo-production.up.railway.app/",
+        vnp_IpAddr: "127.0.0.1",
+        vnp_CreateDate: new Date().toISOString().replace(/[-:TZ.]/g, "").slice(0,14)
+    };
+
+    let signData = qs.stringify(params, { encode: false });
+
+    let hmac = crypto.createHmac("sha512", vnp_HashSecret);
+    let signed = hmac.update(signData).digest("hex");
+
+    params.vnp_SecureHash = signed;
+
+    let paymentUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?" 
+        + qs.stringify(params, { encode: false });
+
+    res.redirect(paymentUrl);
 });
 
 app.listen(3000, () => console.log("Server running"));
